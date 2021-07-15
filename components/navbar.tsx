76a01db94e01fac7a2131ad96bf5eb1324bbe314
Zhixing Zhao <zhixing.zhao@stonybrook.edu>
2021-07-15 09:44:43 -0700

    Navbar Container

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,35 @@
 import 'tailwindcss/tailwind.css'
 import { Menu } from '@headlessui/react'
 
-export default function NavbarContainer() {
+export default function NavbarContainer({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <div className="flex w-screen h-12">
-      <nav className="flex w-screen h-12"></nav>
+    <div className="flex w-screen h-20">
+      <nav className="flex w-screen h-20 bg-green-500"></nav>
+      {children}
     </div>
   )
 }
+
+// /* Name */
+
+// position: absolute;
+// width: 128px;
+// height: 96px;
+// left: -24px;
+// top: -8px;
+
+// font-family: Neuton;
+// font-style: normal;
+// font-weight: normal;
+// font-size: 22px;
+// line-height: 150%;
+// /* or 33px */
+// display: flex;
+// align-items: center;
+// text-align: center;
+
+// color: #000000;
--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,11 @@ import NavbarContainer from '../components/navbar'
 // }
 
 export default function Home() {
-  return <NavbarContainer></NavbarContainer>
+  return (
+    <NavbarContainer>
+      <div></div>
+    </NavbarContainer>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
