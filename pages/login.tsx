7aec80e32cebbebca7dad7afa5b1f80b880ce833
Kyle Huang <kyleghuang@gmail.com>
2021-07-15 15:14:45 -0400

    Created login proto

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+import NavbarContainer from "@components/navbar";
+import LogInContainer from "@components/logincontainer";
+
+export default function LogIn() {
+    return (
+        <NavbarContainer>
+            <LogInContainer></LogInContainer>
+        </NavbarContainer>
+    )
+}
