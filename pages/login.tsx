0a47c6cc262eb6c7861bfb224bf74953b7430e1f
Zhixing Zhao <zhixing.zhao@stonybrook.edu>
2021-07-17 08:18:38 -0700

    Last Common Push V2

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,12 @@
 import NavbarContainer from '@components/navbar'
-import LogInContainer from '@components/logincontainer'
+import LoginCard from '@components/login'
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 
 export default function LogIn() {
   return (
-    <NavbarContainer bg_enabled logged_in>
-      <LogInContainer></LogInContainer>
+    <NavbarContainer bg_enabled logged_in={false} authing>
+      <LoginCard />
     </NavbarContainer>
   )
 }
