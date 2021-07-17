45ae14054db4e7b903f0cddf04cb4851b4a66caf
Jeffery Chen <jefferychen5732@gmail.com>
2021-07-17 10:50:24 -0700

    pulled latest code

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,5 @@
 import NavbarContainer from '@components/navbar'
-<<<<<<< HEAD
-import LogInContainer from '@components/logincontainer'
-=======
 import LoginCard from '@components/login'
->>>>>>> 0a47c6cc262eb6c7861bfb224bf74953b7430e1f
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 
