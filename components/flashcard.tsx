632253d849808db1efcede06a356dc2eb7772839
Kyle Huang <kyleghuang@gmail.com>
2021-07-20 15:26:47 -0400

    Updated animations

--- components/flashcard.tsx
+++ components/flashcard.tsx
@@ -1,45 +1,53 @@
 import 'tailwindcss/tailwind.css'
 import { Fragment, useState } from 'react'
 import { Transition } from '@headlessui/react'
+import { SSL_OP_EPHEMERAL_RSA } from 'constants'
 
 export default function FlashCard() {
   let [isFront, setIsFront] = useState(true)
+
   return (
     <>
       {/*Front Card*/}
       <Transition
         as={Fragment}
         show={isFront}
-        enter="transform transition duration-[400ms]"
-        enterFrom="opacity-0 rotate-[-120deg] scale-50"
-        enterTo="opacity-100 rotate-0 scale-100"
-        leave="transform duration-200 transition ease-in-out"
-        leaveFrom="opacity-100 rotate-0 scale-100 "
-        leaveTo="opacity-0 scale-95"
+        enter="transform duration-200 ease-out transition delay-200"
+        enterFrom="opacity-0 -rotate-180 scale-50"
+        enterTo="opacity-100 -rotate-1 scale-100"
+        leave="transform transition duration-200"
+        leaveFrom="opacity-100 rotate-0 scale-100"
+        leaveTo="opacity-0 rotate-180 scale-50"
       >
-        <div className="flex-inital mt-52 bg-white w-96 h-96 rounded-md shadow-md">
+        <button
+          onClick={() => setIsFront((isFront) => !isFront)}
+          className="flex-inital mt-20 bg-white w-72 h-72 rounded-md shadow-md focus:outline-none"
+        >
           Front
-        </div>
+        </button>
       </Transition>
 
       {/*Back Card*/}
       <Transition
         as={Fragment}
         show={!isFront}
-        enter="transform transition duration-[400ms]"
-        enterFrom="opacity-0 rotate-[-120deg] scale-50"
-        enterTo="opacity-100 rotate-0 scale-100"
-        leave="transform duration-200 transition ease-in-out"
-        leaveFrom="opacity-100 rotate-0 scale-100 "
-        leaveTo="opacity-0 scale-95"
+        enter="transform duration-200 ease-out transition delay-200"
+        enterFrom="opacity-0 -rotate-180 scale-50"
+        enterTo="opacity-100 -rotate-1 scale-100"
+        leave="transform transition duration-200"
+        leaveFrom="opacity-100 rotate-0 scale-100"
+        leaveTo="opacity-0 rotate-180 scale-50"
       >
-        <div className="flex-inital mt-52 bg-white w-96 h-96 rounded-md shadow-md">
+        <button
+          onClick={() => setIsFront((isFront) => !isFront)}
+          className="flex-inital mt-20 bg-white w-72 h-72 rounded-md shadow-md focus:outline-none"
+        >
           Back
-        </div>
+        </button>
       </Transition>
 
       {/*Flip Button*/}
-      <button
+      {/* <button
         onClick={() => setIsFront((isFront) => !isFront)}
         className="font-semibold text-align-center w-20 h-10 text-white 
                     rounded-lg bg-gradient-to-t from-purple-700 to-blue-400
@@ -48,7 +56,7 @@ export default function FlashCard() {
                     cursor-pointer"
       >
         Flip
-      </button>
+      </button> */}
     </>
   )
 }
